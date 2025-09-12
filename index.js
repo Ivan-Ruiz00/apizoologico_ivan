090a9469fceca5c9ec7172a33d673aced72ec101
Nuevas tecnologías <correo@falso.com>
2025-09-11 19:03:50 -0500

    Se inicia el proyecto en express

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-'user strict';
-const http=require('http');
-
-const server=http.createServer(function(req,res){
-    res.writeHead(200,{'content-type':'text/plain'});
-    res.end('Hola mundo!');
-});
-server.listen(5000);
+const express=require('express')
