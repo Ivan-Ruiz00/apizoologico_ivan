3934855027be42a6e6345d9b69887ff581e91a6f
Nuevas tecnologías <correo@falso.com>
2025-09-25 19:36:49 -0500

    Se cambia el index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-const express=require('express');
+const parser=require("body-parser");
+const express=require("express");
 const app=express();
 const port=3000;
+const animalRoutes=require(".routes/animal");
+const mongoose=require("mongoose");
+require("dotenv").config();
 
-app.get('/holamundo',(req,res)=>{
-    res.send('¡Hola mundo!');
-}
+app.use(parser.urlencoded({extended:false}));
+app.use(parser.json());
+
+app.use("/api",animalRoutes);
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("Conexión exitosa")).catch((error)=>console.log(error));
+
+app.listen(
+    port,()=>{
+        console.log(`App de ejemplo escuchando el puerto ${port}`)
+    }
 )
-app.listen(port,()=>{
-    console.log('La aplicación se está ejecutando por el puerto '+`${port}`)
-})
