c38ba1891d41c52cd0532f02b2c48e2cb907e710
JavierAlmonacid-10 <javialmonacidtorres@gmail.com>
2025-10-16 19:26:05 -0500

    Se crean los queries con operadores

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -16,6 +16,55 @@ router.get(
 );
 
 router.get(
+    "/animalitos/igual/:edad",(req,res)=>{
+        let { edad } = req.params;
+        animalSchema.find({edad:{$eq:edad}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    }
+);
+
+router.get(
+    "/animalitos/noigual/:edad",(req,res)=>{
+        let { edad } = req.params;
+        animalSchema.find({edad:{$ne:edad}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    }
+);
+
+router.get(
+    "/animalitos/greater/:edad",(req,res)=>{
+        let { edad } = req.params;
+        animalSchema.find({edad:{$gt:edad}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    }
+);
+
+router.get(
+    "/animalitos/greatequal/:edad",(req,res)=>{
+        let { edad } = req.params;
+        animalSchema.find({edad:{$gte:edad}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    }
+);
+
+router.get(
+    "/animalitos/less/:edad",(req,res)=>{
+        let { edad } = req.params;
+        animalSchema.find({edad:{$lt:edad}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    }
+);
+
+router.get(
+    "/animalitos/lessequal/:edad",(req,res)=>{
+        let { edad } = req.params;
+        animalSchema.find({edad:{$lte:edad}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    }
+);
+
+router.get(
+    "/animalitos/in/:edad",(req,res)=>{
+        let { edad } = req.params;
+        animalSchema.find({edad:{$in:edad}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
+    }
+);
+
+router.get(
     "/animalitos/:id", (req, res) => {
         const { id } = req.params;
         animalSchema
